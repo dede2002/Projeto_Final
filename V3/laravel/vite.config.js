6ace0c9e3c104eecc3de2d92843e4823550f4f15
Jose <you@example.com>
2024-05-28 14:44:50 -0300

    Docker integrado no projeto
    Realizei algumas alterações e o docker agora está integrado em nosso projeto.

--- V3/laravel/vite.config.js
+++ V3/laravel/vite.config.js
@@ -4,17 +4,22 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
     ],
+    server: {
+        host: '0.0.0.0', // Permite que o servidor seja acessível externamente
+        port: 3000,      // Define a porta padrão do Vite
+        hmr: {
+            host: 'localhost', // Host para o Hot Module Replacement
+        },
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js'
+        }
+    }
 });

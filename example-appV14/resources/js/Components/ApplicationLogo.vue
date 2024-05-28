af02085626c73208e6c220474569c3b9a5619d70
Joueph <111884490+Joueph@users.noreply.github.com>
2024-05-28 16:12:25 -0300

    Delete example-appV14 directory
    Deletando a versão antiga

--- example-appV14/resources/js/Components/ApplicationLogo.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <svg viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg">
-        <image href="https://pasmars.com/wp-content/uploads/2024/05/Duhaclinica.svg" height="100%" width="100%"/>
-    </svg>
-</template>
